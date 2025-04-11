c86259971e46fa3689349c26b5e4dfc5c856dd51
Daniel Kuehn <daniel@kuehn.foo>
2025-04-10 22:05:39 -0400

    Automate tag message in bump script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 NEW_VERSION="$(<./VERSION.txt)"
+OLD_TAG="$(git tag --sort=-creatordate | head -n1)"
 
 sed -i -E 's/RUNEPRICE_VERSION="[0-9]+\.[0-9]+\.[0-9]+"/RUNEPRICE_VERSION="'"$NEW_VERSION"'"/' ./runeprice.sh
 
@@ -19,5 +20,13 @@ read -rp "Commit and push? (y/N): " CHOICE
 
 git add ./runeprice.sh ./VERSION.txt
 git commit -m "$NEW_VERSION"
-git tag "$NEW_VERSION"
+
+git tag "$NEW_VERSION" -F - <<EOF
+$NEW_VERSION
+
+Full Changelog:
+
+https://github.com/danny-kuehn/runeprice/compare/$OLD_TAG...$NEW_VERSION"
+EOF
+
 git push --follow-tags
